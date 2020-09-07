bdbddf9d388d25b4737e87ba6a8f73310129dafd
Katherine Shimkus <linarespachecoerick@gmail.com>
2020-09-06 21:59:09 -0400

    Displays data from backend to the frontend TODO: add createProduct route with a form // will be used for update TODO: Connect backend and frontend with proxy

---
+++ src/components/Homepage.component.js
@@ -0,0 +1,11 @@
+import React  from 'react';
+
+function Homepage(){
+
+    
+        return(
+            <div className='container'>Welcome!</div>
+        )
+}
+
+export default Homepage
--- src/components/information.component.js
+++ src/components/information.component.js
@@ -1,5 +1,5 @@
 import React from "react";
-class Products extends React.Component {
+class Information extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -14,25 +14,22 @@ class Products extends React.Component {
       .catch((err) => console.log(err));
   }
   callApi = async () => {
-    const response = await fetch("http://localhost:5000/products/");
+    const response = await fetch("http://localhost:5000/info/");
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
     return body;
   };
 
   render() {
-    let products = this.state.response
-
-    console.log({products})
+    let info = this.state.response
+    console.log({info})
     return (
       <div>
-
-    <div><strong>TODO: add EDIT, UPDATE, and DELETE functionality for each product</strong></div>
-          {products.map((product) =>
-            <p key={product._id}>{product.name}</p>
+          {info.map((info) =>
+            <p key={info._id}>{info.username}</p>
           )}
       </div>
     );
   }
 }
-export default Products;
+export default Information;
