5090748cd2787083765830659fee741ff06eabdb
crkingsley <colleen.kingsley89@gmail.com>
2020-09-12 12:11:13 -0400

    Implement subdocument schema, and begin stateless function

--- backend/models/products.model.js
+++ backend/models/products.model.js
@@ -1,76 +1,86 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productsSchema = new Schema(
+const optionsSchema = new Schema(
   {
-    sku: String,
-    quantity:Number,
+    type: String,
     name: String,
     nameTranslated: {
       en: String,
       es: String
-    },
-    price:Number,
-    compareToPrice:Number,
-    isShippingRequired:Boolean,
-    categoryIds: [
+  }
+});
+
+const shippingSchema = new Schema(
+  {
+    type: String,
+    methodMarkup: Number,
+    flatRate: Number,
+    disabledMethods: [
       Array
     ],
-    weight:Number,
-    enabled:Boolean,
-    description: String,
-    productClassId: Number,
-    created:Date,
-    fixedShippingRateOnly:Boolean,
-    fixedShippingRate:Number,
-    tax: {
-      enabledManualTaxes: [
-        String
-      ]
-    },  
-    options: [
-      {
-        type: String,
-        name:String,
-        nameTranslated: {
-          en: String,
-          es: String
-        },
-        choices: [
-          {
-            text:String,
-            textTranslated: {
-              en: String,
-              es:String
-            },
-            priceModifier:Number,
-            priceModifierType: Number
-          },
-          {
-            text: String,
-            textTranslated: {
-              en: String,
-              es:String
-            },
-            priceModifier: Number,
-            priceModifierType:String
-          }
-        ],
-          defaultChoice:Number,
-          required: Boolean
-      }
-    ],
-    shipping: {
-      type: String,
-      methodMarkup:Number,
-      flatRate: Number,
-      disabledMethods: [
-        Array
-      ],
-      nabledMethods: [Array]
-    }
+    enabledMethods: [Array]
+  });
+
+const choicesSchema = new Schema(
+  {
+    text: String,
+    textTranslated: {
+      en: String,
+      es: String
+    },
+    priceModifier: Number,
+    priceModifierType: Number
+  },
+  {
+    text: String,
+    textTranslated: {
+      en: String,
+      es: String
+    },
+    priceModifier: Number,
+    priceModifierType: String
+  },
+  {
+  defaultChoice: Number,
+  required: Boolean
+    });
+
+const productsSchema = new Schema({
+  sku: String,
+  quantity: Number,
+  name: String,
+  nameTranslated: {
+    en: String,
+    es: String
+  },
+  price: Number,
+  compareToPrice: Number,
+  isShippingRequired: Boolean,
+  categoryIds: [Number],
+  weight: Number,
+  enabled: Boolean,
+  description: String,
+  productClassId: Number,
+  created: String,
+  fixedShippingRateOnly: Boolean,
+  fixedShippingRate: Number,
+  brand: String,
+  tax: {
+    enabledManualTaxes: [
+      Number
+    ]
+  },
+  options: [
+    optionsSchema
+  ],
+  choices: [
+    choicesSchema
+  ],
+  shipping: {
+    shippingSchema
   }
-);
+});
 
 const Products = mongoose.model("Products", productsSchema);
 
